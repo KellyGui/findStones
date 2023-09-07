365e23d79900d28dde82ec6129f3598cfdbfd538
KellyGui <mengpinggui@gmail.com>
2023-09-07 10:28:10 +0800

    commit

--- build/3dparty/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/gmp/findStones/3dparty/googletest/googletest
-# Build directory: /data/gmp/findStones/build/3dparty/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
